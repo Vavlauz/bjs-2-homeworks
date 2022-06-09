a61b675c18dfb918ca798a7fbf9ab9dda7aafc21
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-06-09 23:09:56 +0700

    add code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,35 +1,71 @@
 "use strict";
 function solveEquation(a, b, c) {
-  let res = {};
+  let res = [];
   if (a == 0) {
     return false;
   }
 
   let D = Math.pow(b, 2) - 4 * a * c;
-  console.log("D = " + D);
 
   if (D < 0) {
-    return false;
+    return res;
   }
 
-  res["discriminant"] = D;
-
   if (D == 0) {
-    res["quadratic roots"] = (-b + Math.sqrt(D)) / (2 * a);
+    let tmp = [];
+    tmp.push(-b / (2 * a));
+    res = tmp;
   } else if (D > 0) {
     let tmp = [];
     tmp.push((-b + Math.sqrt(D)) / (2 * a));
     tmp.push((-b - Math.sqrt(D)) / (2 * a));
-    res["quadratic roots"] = tmp;
+    res = tmp;
   }
   return res;
 }
-console.log(solveEquation(1, 4, 1));
+console.log(solveEquation(1, 2, 1));
 
-// function calculateTotalMortgage(percent, contribution, amount, date) {
-//   let totalAmount;
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+  let monthRate;
+  let sum;
 
-//   // код для задачи №2 писать здесь
+  let dateNow = new Date();
+  let dateEnd = new Date(date);
+  let diffDate = dateEnd - dateNow;
+  // let milliseconds = diffDate;
+  // let seconds = milliseconds / 1000;
+  // let minutes = seconds / 60;
+  // let hours = minutes / 60;
+  // let days = hours / 24;
+  // let months = +(days / 30).toFixed(0);
+  let months = Math.trunc(
+    +(diffDate / (1000 * 60 * 60 * 24 * 30.5)).toFixed(0)
+  );
+  if (percent < 0) {
+    console.log(
+      'Параметр "Процентная ставка" содержит неправильное значение "test"'
+    );
+  } else {
+    monthRate = percent / (100 * 12);
+  }
 
-//   return totalAmount;
-// }
+  if (contribution < 0) {
+    console.log(
+      'Параметр "Начальный взнос" содержит неправильное значение "test"'
+    );
+  } else if (amount < 0) {
+    console.log(
+      'Параметр "Общая стоимость" содержит неправильное значение "test"'
+    );
+  } else {
+    sum = amount - contribution;
+  }
+  let payment =
+    sum * (monthRate + monthRate / (Math.pow(monthRate + 1, months) - 1));
+  totalAmount = +(payment * months).toFixed(2);
+  console.log(totalAmount);
+
+  return totalAmount;
+}
+console.log(calculateTotalMortgage(1, 0, -1, "06/09/2025"));
