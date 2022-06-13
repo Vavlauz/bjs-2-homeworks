9974517f1e9a96d7de954ad679feb0c7bf025873
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-06-13 10:37:58 +0700

    add code

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,94 @@
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
 
-  // Ваш код
+let array = [];
+(minimum = -100), (maximum = 101);
+
+for (var i = minimum; i < maximum; i++) {
+  array.push(i);
+}
+
+// console.log(array);
+
+function getArrayParams(arr) {
+  (min = null), (max = null), (sum = 0), (avg = null);
+
+  for (let ar of arr) {
+    if (ar <= arr[0]) {
+      min = ar;
+    }
+    if (ar >= arr[0]) {
+      max = ar;
+    }
+  }
+
+  for (let ar in arr) {
+    sum = sum + arr[ar];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
+// console.log(getArrayParams([-99, 99, 10, 105]));
 
 // Задание 2
-function worker(arr) {
-  let sum;
 
-  // Ваш код
+function worker(arr) {
+  let sum = 0;
+
+  for (let ar in arr) {
+    sum = sum + arr[ar];
+  }
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = null;
+  let sum = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let ar in arrOfArr) {
+    sum = func(arrOfArr[ar]);
+    if (sum >= max) {
+      max = sum;
+    }
+  }
+
   return max;
 }
+// console.log(
+//   makeWork(
+//     [
+//       [1, 2, 3, 4],
+//       [10, 20, -10, -20],
+//     ],
+//     worker
+//   )
+// );
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = null;
+  let min = null;
+  let diff = null;
+
+  for (let ar of arr) {
+    if (ar <= arr[0]) {
+      min = ar;
+    }
+    if (ar >= arr[0]) {
+      max = ar;
+    }
+  }
+  return Math.abs((diff = max - min));
 }
+
+// console.log(
+//   makeWork(
+//     [
+//       [-10, -20, -40],
+//       [10, 20, 30],
+//     ],
+//     worker2
+//   )
+// );
