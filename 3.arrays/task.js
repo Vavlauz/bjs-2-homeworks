bc35a5f6b5d4b5254e8acbe83319b602fefd9232
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-06-15 19:05:22 +0700

    add code

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  result =
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index]);
 
   return result; // boolean
 }
+// console.log(compareArrays([8, 9], [8, 9]));
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+    .filter((element) => element >= 0)
+    .filter((element) => element % 3 === 0)
+    .map((element) => element * 10);
 
   return resultArr; // array
 }
+// console.log(advancedFilter([-1, -2]));
