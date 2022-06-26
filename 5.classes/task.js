ce9b85ab1b9b5524d1ed9715475949f61d371b9c
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-06-26 17:17:08 +0700

    add code

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -24,20 +24,20 @@ class PrintEditionItem {
   }
 
   fix() {
-    return (this.state = this._state * 1.5);
+    return (this.state *= 1.5);
   }
 }
 
-// const sherlock = new PrintEditionItem(
-//   "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
-//   2019,
-//   1008
-// );
+const sherlock = new PrintEditionItem(
+  "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+  2019,
+  1008
+);
 
-// console.log(sherlock.releaseDate); //2019
-// console.log(sherlock.state); //100
-// sherlock.fix();
-// console.log(sherlock.state); //100
+console.log(sherlock.releaseDate); //2019
+console.log(sherlock.state); //100
+sherlock.fix();
+console.log(sherlock.state); //100
 
 class Magazine extends PrintEditionItem {
   constructor(name, releaseDate, pagesCount) {
@@ -88,6 +88,8 @@ console.log(picknick.state); //10
 picknick.fix();
 console.log(picknick.state); //15
 
+// 2 задание
+
 class Library {
   constructor(name) {
     this.name = name;
@@ -96,28 +98,24 @@ class Library {
 
   addBook(book) {
     if (book.state > 30) {
-      this.books.push(book);
-    } else {
-      return null;
+      if (!this.books) {
+        this.books = [book];
+      } else {
+        this.books.push(book);
+      }
     }
+    return this.books;
   }
 
   findBookBy(type, value) {
-    let search = this.books.find((el) => el[type] == value);
-    if (search !== undefined) {
-      return search;
-    } else {
-      return null;
-    }
+    const book = this.books.find((item) => item[type] === value);
+    return book || null;
   }
 
   giveBookByName(bookName) {
-    let search = this.books.filter((el) => el.name !== bookName);
-    if (search !== undefined) {
-      return (this.books = search);
-    } else {
-      return null;
-    }
+    let book = this.books.find((el) => el.name === bookName);
+    this.books.splice(this.books.indexOf(book), 1);
+    return book || null;
   }
 }
 
@@ -147,5 +145,68 @@ console.log(library.findBookBy("name", "Властелин колец")); //null
 console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
 
 console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
-library.giveBookByName("Машина времени");
+console.log(library.giveBookByName("Машина времени"));
 console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+
+// 3 задание
+
+class Student {
+  constructor(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.journal = {};
+  }
+
+  addMark(grade, subject) {
+    if (grade < 1 || grade > 5 || typeof grade !== "number") {
+      return console.log(`Ошибка, оценка должна быть числом от 1 до 5`);
+    }
+    if (this.journal[subject] === undefined) {
+      this.journal[subject] = [];
+    }
+    return this.journal[subject].push(grade);
+  }
+
+  getAverageBySubject(subject) {
+    if (this.journal[subject]) {
+      let averageMark = 0;
+      if (this.journal[subject] !== undefined) {
+        if (this.journal[subject].length > 0) {
+          let sum = 0;
+          this.journal[subject].forEach((mark) => {
+            sum += mark;
+          });
+          averageMark = sum / this.journal[subject].length;
+        }
+      }
+      return averageMark;
+    }
+    return console.log("Несуществующий предмет");
+  }
+
+  getAverage() {
+    let sumOfAveMarks = 0;
+    let countOfSubjects = 0;
+    Object.entries(this.journal).forEach(([subject]) => {
+      sumOfAveMarks += this.getAverageBySubject(subject);
+      countOfSubjects++;
+    });
+    return sumOfAveMarks / countOfSubjects;
+  }
+
+  exclude() {
+    return console.log("Исключен за попытку подделать оценки");
+  }
+}
+
+const student = new Student("Олег Никифоров");
+student.addMark(5, "algebra");
+student.addMark(5, "algebra");
+student.addMark(5, "geometry");
+student.addMark(4, "geometry");
+student.addMark(6, "geometry"); // "Ошибка, оценка должна быть числом от 1 до 5"
+student.getAverageBySubject("geometry"); // Средний балл по предмету geometry 4.5
+student.getAverageBySubject("biology"); // Несуществующий предмет
+student.getAverage(); // Средний балл по всем предметам 4.75
+student.exclude("Исключен за попытку подделать оценки");
