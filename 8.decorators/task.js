e0bef7a97fcc676dac68a7f3a54e6e825072ef47
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-07-09 23:04:16 +0700

    add code

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -45,6 +45,7 @@ function debounceDecoratorNew(func, ms) {
 
     timerid = setTimeout(() => {
       func.call(this, ...args);
+      value = false;
     }, ms);
   }
   return wrapper;
@@ -77,12 +78,14 @@ function debounceDecorator2(func, ms) {
     timerid = setTimeout(() => {
       func.call(this, ...args);
       wrapper.count++;
+      value = false;
     }, ms);
   }
   return wrapper;
 }
 
 const sendSignal2 = () => console.log("Сигнал2 отправлен");
+const sendSignal3 = () => console.log(upgradedSendSignal2.count);
 const upgradedSendSignal2 = debounceDecorator2(sendSignal2, 2000);
 setTimeout(upgradedSendSignal2); // Сигнал отправлен
 setTimeout(upgradedSendSignal2, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
@@ -91,4 +94,4 @@ setTimeout(upgradedSendSignal2, 1200); // проигнорировано так 
 setTimeout(upgradedSendSignal2, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
 setTimeout(upgradedSendSignal2, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
 setTimeout(upgradedSendSignal2, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
-console.log(upgradedSendSignal2.count);
+setTimeout(sendSignal3, 7000);
